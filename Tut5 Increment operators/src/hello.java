498d1f6d14192015fec193c9a999a7e9b27898ea
charlesdelaney2 <51384235+charlesdelaney2@users.noreply.github.com>
2019-06-05 23:48:19 +0100

    Increment operators
    To increment operators and change the value by adding, subtracting 1 and also to multiple or divide

---
+++ Tut5 Increment operators/src/hello.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+class hello
+{
+	public static void main(String args[]) {
+		Scanner bucky = new Scanner(System.in);
+		
+		int tuna = 5;
+		int bass = 18;
+		
+		tuna +=5;
+		++tuna;
+		System.out.println(tuna);
+		System.out.println(tuna++);
+		System.out.println(tuna);
+		System.out.println(--tuna);
+		
+	}
+}
